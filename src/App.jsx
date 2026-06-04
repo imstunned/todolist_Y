f84d1b5f3747b421ee06ed69061ab0630841d7a8
gl1dless <tr1p_exe@mail.ru>
2026-06-04 18:21:57 +0300

    staaart

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
     }
 
     async function fetchPoeNinjaData() {
-      const poeResponse = await axios.get('http://localhost:3001/api/poe-currency', {
+      const poeResponse = await axios.get('https://todolist-y.onrender.com/api/poe-currency', {
         params: {
           league: POE_LEAGUE,
         },
