f6f2587dfdfbfe6d7409c7575b581f6e383dbc8c
gl1dless <tr1p_exe@mail.ru>
2026-06-04 18:32:43 +0300

    buildig...

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/todolist_Y/',
-  server: {
-    proxy: {
-      '/poe-ninja': {
-        target: 'https://poe.ninja',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/poe-ninja/, ''),
-      },
-    },
-  },
 });
